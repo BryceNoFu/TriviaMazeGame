784336b59f60e02ea8604ad20c5968486c60e916
Bryce Nobutoshi Fukuda <brycefukuda@gmail.com>
2021-05-16 22:55:05 -0700

    create question interface and abstract general question

--- src/questions/Question.java
+++ src/questions/Question.java
@@ -1,4 +1,37 @@
 package questions;
 
+/**
+ * Abstract class for question classes.
+ *
+ * @author Bryce Fukuda
+ */
 public interface Question {
+
+    /**
+     * Getter method for id.
+     *
+     * @return question id
+     */
+    int getId();
+
+    /**
+     * Getter method for question text.
+     *
+     * @return question text
+     */
+    String getQuestion();
+
+    /**
+     * Getter method for answer.
+     *
+     * @return answer text
+     */
+    String getAnswer();
+
+    /**
+     * Getter method for hint.
+     *
+     * @return hint text
+     */
+    public String getHint();
 }
---
+++ src/questions/QuestionGeneral.java
@@ -0,0 +1,78 @@
+package questions;
+
+/**
+ * Abstract class for question classes, containing the minimum
+ * required methods that will be implemented for these questions.
+ *
+ * @author Bryce Fukuda
+ */
+public abstract class QuestionGeneral implements Question {
+    // fields
+    /**
+     * Question id, 1 to 100.
+     */
+    private int id;
+
+    /**
+     * Question text to ask the player.
+     */
+    private String question;
+
+    /**
+     * Answer to the question.
+     */
+    private String answer;
+
+    /**
+     * Hint to the question.
+     */
+    private String hint;
+
+    // constructors
+    public QuestionGeneral(int id, String question, String answer, String hint) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+        this.hint = hint;
+    }
+
+    // methods
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getAnswer() {
+        return answer;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getHint() {
+        return hint;
+    }
+
+    /**
+     * Creates string of id, question, any choices, answer, and hint.
+     *
+     * @return String of all fields for question.
+     */
+    public abstract String toString();
+}
