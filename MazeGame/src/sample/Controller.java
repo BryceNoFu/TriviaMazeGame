b9abda2153e9e112a86d39e95d2ea2a0c5c073f4
Trương Thành Tiến <thanhtien7596@MacBook-Pro-cua-Truong.local>
2021-05-24 21:27:32 -0700

    Update GUI

--- MazeGame/src/sample/Controller.java
+++ MazeGame/src/sample/Controller.java
@@ -1,4 +1,82 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    double x = 0;
+    double y = 0;
+    private Node node;
+    private Stage stage;
+    private Scene scene;
+
+    @FXML
+    void dragged (MouseEvent event) {
+        node = (Node) event.getSource();
+        stage = (Stage) node.getScene().getWindow();
+        stage.setX(event.getSceneX() + x);
+        stage.setY(event.getSceneY() +y);
+    }
+    @FXML
+    void pressed(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+
+    }
+    @FXML
+    void quit(MouseEvent event) {
+        System.exit(0);
+    }
+    @FXML
+    void switchToScene1 (ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        node = (Node) event.getSource();
+        stage = (Stage) node.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void switchToNewGameScene (ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("NewGameScene.fxml"));
+        node = (Node) event.getSource();
+        stage = (Stage) node.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void switchToAboutScene (ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("About.fxml"));
+        node = (Node) event.getSource();
+        stage = (Stage) node.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void mouseEntered(MouseEvent event) {
+
+
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
+
+
 }
